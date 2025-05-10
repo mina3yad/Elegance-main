6ac88c58153fd175533f86a5bc1a32ce212c9626
Mina_3yad <minaayad20005@gmail.com>
2025-05-11 01:07:16 +0300

    Update

--- Elegance-main/javascript/home.js
+++ Elegance-main/javascript/home.js
@@ -24,12 +24,12 @@ function updateIcon() {
         muteIcon.classList.add('fa-volume-up');
     }
 }
-// Auto-scroll for product carousel
+
 const carousel = document.getElementById('auto-carousel');
 
 let scrollAmount = 0;
-const scrollStep = 2; // سرعة الحركة
-const scrollDelay = 20; // الفاصل الزمني بين كل حركة (ms)
+const scrollStep = 2; 
+const scrollDelay = 20; 
 
 function autoScrollCarousel() {
     if (carousel.scrollWidth - carousel.clientWidth <= scrollAmount) {
